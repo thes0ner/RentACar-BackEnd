0a802049af73c2c0b7e3b59245376896b4eb8aa6
Soner <thes0ner@outlook.co>
2025-02-22 01:05:29 +0100

    .

--- RentACar.Entities/Concrete/Customer.cs
+++ RentACar.Entities/Concrete/Customer.cs
@@ -11,7 +11,6 @@ namespace RentACar.Entities.Concrete
     public class Customer
     {
 
-        // customer properties
 
         public int Id { get; set; }
         public string FirstName { get; set; }
