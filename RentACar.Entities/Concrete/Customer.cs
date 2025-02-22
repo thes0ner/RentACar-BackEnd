541e576c852fb5ab42ccf3160686ef44bd6ebf76
Soner Abdürrahman <thes0ner@outlook.com>
2025-02-22 01:03:07 +0100

    .

--- RentACar.Entities/Concrete/Customer.cs
+++ RentACar.Entities/Concrete/Customer.cs
@@ -10,6 +10,7 @@ namespace RentACar.Entities.Concrete
 {
     public class Customer
     {
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
